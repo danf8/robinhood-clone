9eb55982a1218484a1d17e1d23d26bc7f5aee1f3
Daniel Fields <dgfields4193@gmail.com>
2023-05-23 13:21:01 -0400

    wip: add nav links and logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,25 @@
-import {GiRobinHoodHat} from 'react-icons/gi'
-
+// import {GiRobinHoodHat} from 'react-icons/gi'
+import Logo from '../robinhood-logo.svg'
+import '../css/header.css';
 const Header = () => {
     return(
         <div className="header__wrap">
             <div className="logo_head">
-                <GiRobinHoodHat/>
+                {/* <GiRobinHoodHat/> */}
+                <img src={Logo} width={25} />
             </div>
             <div className="head__search">
                 <div className='header__searchContainer'>
-                    
+                    <input type="text" placeholder='Search' />
                 </div>
             </div>
+                <div className="header__menuItem">
+                    <a href="#">Free Stocks</a>
+                    <a href="#">Portfolio</a>
+                    <a href="#">Cash</a>
+                    <a href="#">Messages</a>
+                    <a href="#">Account</a>
+                </div>
         </div>
     )
 }
