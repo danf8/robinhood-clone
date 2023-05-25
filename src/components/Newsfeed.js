ebd88d0eac6e0404a4c38842aae3f5baa87087d9
Daniel Fields <dgfields4193@gmail.com>
2023-05-25 14:34:47 -0400

    wip: add timeline & popular sections

--- src/components/Newsfeed.js
+++ src/components/Newsfeed.js
@@ -1,7 +1,23 @@
 import '../css/newsfeed.css'
 import Linegraph from './Linegraph';
-
+import TimeLine from './TimeLine';
+import { Chip, Avatar } from '@mui/material';
+import { useState } from 'react';
 const Newsfeed = () => {
+
+    const [popularTopics, setTopics] = useState([
+        "Technology",
+        "Top Movies",
+        "Upcoming Earnings",
+        "Crypto",
+        "Cannabis",
+        "Healthcare Supplies",
+        "Index ETFs",
+        "Technology",
+        "China",
+        "Pharma",
+      ]);
+
     return(
         <div className="newsfeed">
             <div className="newsfeed__container">
@@ -12,6 +28,35 @@ const Newsfeed = () => {
                     </div> 
                     <div className="newsfeed__chart">
                         <Linegraph />
+                        <TimeLine />
+                    </div>
+                </div>
+                <div className="newsfeed__buying__section">
+                    <h2>Buying Power</h2>
+                    <h2>$4.11</h2>
+                </div>
+                <div className="newsfeed__market__section">
+                    <div className="newsfeed__market__box">
+                        <p>Markets closed</p>
+                        <h1>happy holidays</h1>
+                    </div>
+                </div>
+                <div className="newsfeed__popularlists__section">
+                    <div className="newsfeed__popularlists__intro">
+                        <h1>Popular List</h1>
+                        <p>Show More</p>
+                    </div>
+                    <div className="newsfeed_popularlists_badges">
+                        {popularTopics.map((topic) => (
+                            <Chip 
+                                className="topic__bade"
+                                variant="outlinded"
+                                label={topic}
+                                avatar={<Avatar
+                                    src={`https://avatars.dicebear.com/api/human/${topic}.svg`}
+                                />} 
+                            />
+                        ))}
                     </div>
                 </div>
             </div>
